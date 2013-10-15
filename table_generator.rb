8b47732c905d5c172581e818b292e6cfae69d973
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-10-15 08:16:44 +0100

    Remove student names. Replace with numbers so it's anonymous

--- table_generator.rb
+++ table_generator.rb
@@ -3,14 +3,17 @@ require 'mongo'
 collection = Mongo::Connection.new("localhost", 27017).db("logic_survey")["survey_results"]
 
 result = ""
+i = 1
 collection.find.each do |doc|
   result += "<tr>\n"
-  result += "<td class=\"student\">#{doc['student']}</td>\n"
+  # result += "<td class=\"student\">#{doc['student']}</td>\n"
+  result += "<td class=\"student\">#{i}</td>\n"
   result += "<td >#{doc['responses'][0]}</td>\n\n"
   result += "<td>#{doc['responses'][1]}</td>\n\n"
   result += "<td>#{doc['responses'][2]}</td>\n\n"
   result += "<td>#{doc['responses'][3]}</td>\n\n"
   result += "</tr>"
+  i += 1
 end
 
 File.open("table.html", "w") do |file|
